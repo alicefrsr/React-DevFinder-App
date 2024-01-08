8aa875af333399710511994ad32994e77146c58d
alicefrsr <94556600+alicefrsr@users.noreply.github.com>
2024-01-08 15:42:01 +0100

    fixing responsive

--- src/components/CardBg.js
+++ src/components/CardBg.js
@@ -4,10 +4,8 @@ import '../styles/CardBg.css';
 // import { ThemeContext } from '../contexts/themeContext';
 import { useTheme } from '../contexts/themeContext';
 
-const CardBg = props => {
+const CardBg = (props) => {
   const { theme } = useTheme();
-  // console.log('theme from CARD BG:', theme);
-
   return <main className={`bg-card ${theme}`}>{props.children}</main>;
 };
 
